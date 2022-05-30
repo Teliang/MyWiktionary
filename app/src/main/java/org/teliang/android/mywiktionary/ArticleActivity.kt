6cba01038005d7f26bddf8532e0585d9ec1c277b
teliang <telianghung@outlook.com>
2022-05-30 14:50:45 +0800

    Web view support back operation

--- app/src/main/java/org/teliang/android/mywiktionary/ArticleActivity.kt
+++ app/src/main/java/org/teliang/android/mywiktionary/ArticleActivity.kt
@@ -3,6 +3,7 @@ package org.teliang.android.mywiktionary
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -21,4 +22,17 @@ class ArticleActivity : AppCompatActivity() {
         Log.d(TAG, word.toString())
         myWebView.loadUrl("https://$language.m.wiktionary.org/wiki/$word")
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        // Check if the key event was the Back button and if there's history
+        val myWebView: WebView = findViewById(R.id.article_webview)
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
+    }
+
 }
--- app/src/main/java/org/teliang/android/mywiktionary/MainActivity.kt
+++ app/src/main/java/org/teliang/android/mywiktionary/MainActivity.kt
@@ -1,6 +1,7 @@
 package org.teliang.android.mywiktionary
 
 import android.os.Bundle
+import android.view.KeyEvent
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
@@ -13,4 +14,17 @@ class MainActivity : AppCompatActivity() {
         myWebView.loadUrl("https://www.wiktionary.org/")
         myWebView.webViewClient = WebViewClient()
     }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        val myWebView: WebView = findViewById(R.id.main_webview)
+        // Check if the key event was the Back button and if there's history
+        if (keyCode == KeyEvent.KEYCODE_BACK && myWebView.canGoBack()) {
+            myWebView.goBack()
+            return true
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event)
+    }
+
 }
